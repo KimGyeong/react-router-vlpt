54f0fbc1c4903b944d9580127a7f585231e9927f
hwangbo <taekwonod714@naver.com>
2020-06-30 16:07:31 +0900

    feat: 서브라우트

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from "./Home";
 import {Link, Route} from "react-router-dom";
 import About from "./About";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
     // /about 경로가 / 규칙과 일치하기에 exact라는 property를 true로 설정하면 됨.
@@ -15,11 +15,14 @@ function App() {
                 <li>
                     <Link to="/about">소개</Link>
                 </li>
+                <li>
+                    <Link to="/profiles">프로필 목록</Link>
+                </li>
             </ul>
             <hr />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
-            <Route path="/profiles/:username" component={Profile} />
+            <Route path="/profiles" component={Profiles} />
         </div>
     );
 }
